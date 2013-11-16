bc5e54a6d368f716d4795f339543aa8172e4e226
Matt Oliver <protogonoi@gmail.com>
2013-11-16 13:34:14 +1100

    Changed to using Visual Studio 2012. Required modifying config header so that is does not include fenv.h

--- SMC/soxr-config.h
+++ SMC/soxr-config.h
@@ -8,7 +8,7 @@
 #define HAVE_DOUBLE_PRECISION 1
 #define HAVE_AVFFT            0
 #define HAVE_SIMD             1
-#define HAVE_FENV_H           1
+#define HAVE_FENV_H           0
 #define HAVE_LRINT            1
 #define WORDS_BIGENDIAN       0
 
